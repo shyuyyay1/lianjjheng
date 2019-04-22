2899a3a2f0bbcc18783d1e66555a4879268588e1
shyuyyay1 <49711194+shyuyyay1@users.noreply.github.com>
2019-04-23 07:54:20 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,3 @@
+<?php 
+	$db=new mysqli('lianggheng.nctu.me/phpmyadmin/sql.php','root','root','website');
+	?>
--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 
+
+<?php
+    require_once 'db/connect.php';
+    if(isset($_GET['searched'])){
+        $keywords=$db->searched($_GET['searched']);
+        $query =$db->query("
+            SELECT lastname,phone
+            FROM WERE lastname'%{$searched}%'OR phone LIKE '%{searched}%'
+            ");
+            ?>
+            <div class="result-count">
+            found<?php echo $query->num_rows; ?> results.
+        </div>
+        <?php
+            if($query->num_rows){
+                while($r=$query->fetch_object()) {
+                    ?>
+                    <div class="result">
+                        <a href="#"><?php echo $r->lastname; ?></a>
+
+
+
 <!doctype html>
 <html lang="en">
 <head>
